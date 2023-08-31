45c347ec95f5bd49a361a18ea0513c985d338150
Kåre Kejser <kaaj@itu.dk>
2023-08-31 16:18:26 +0200

    Adding cheep option to weekly assignment

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,23 +1,27 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
 using Microsoft.Win32.SafeHandles;
+
+var filename = "data/chirp_cli_db.csv";
 
 if (args[0] == "read")
 {
     // The following is adapted from https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-read-text-from-a-file
     try
     {
-        var reader = new StreamReader("data/chirp_cli_db.csv");
+        var reader = new StreamReader(filename);
         var cheeps = new List<string>();
         reader.ReadLine();
         while (reader.Peek() >= 0)
         {
+            Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
             string line = reader.ReadLine();
-            string[] words = line.Split(',');
+            string[] words = CSVParser.Split(line);
             string author = words[0];
             string message = words[1];
             int timestamp = int.Parse(words[2]);
-            var time = DateTimeOffset.FromUnixTimeSeconds(timestamp);
+            var time = DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime();
             string output = $"{author} @ {time}: {message}";
             cheeps.Add(output);
         }
@@ -29,6 +33,22 @@ if (args[0] == "read")
     catch (IOException e)
     {
         Console.WriteLine("The file could not be read");
+        Console.WriteLine(e.Message);
+    }
+}
+else if (args[0] == "cheep")
+{
+    try
+    {
+        string user = "kaaj";
+        var time = DateTime.Now;
+        long unixTime = ((DateTimeOffset)time).ToUnixTimeSeconds();
+        string input = $"{user},\"{args[1]}\",{unixTime}";
+        File.AppendAllText(filename, Environment.NewLine + input);
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file does not exist");
         Console.WriteLine(e.Message);
     }
 }
